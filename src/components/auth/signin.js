13ba271e9df85e4110e1ece1386e32c689a5d520
Nick Brunal <nickbrunal@gmail.com>
2019-10-08 00:54:32 -0400

    built sign in and sign out routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div className='sign-in'>
+                sign in
+            </div>
+        )
+    }
+}
+
+export default SignIn;
+
+// import React, { Component } from 'react';
+
+// class SignIn extends Component {
+//     render() {
+//         return {
+//             < div className = 'sign-in' >
+//             sign innn
+//             </div >
+//         }
+// }
+// }
+
+// export default SignIn;
