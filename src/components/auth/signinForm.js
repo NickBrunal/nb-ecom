f42cfd56f0f75df54ab547ca780f3180257333c2
Nick Brunal <nickbrunal@gmail.com>
2019-10-11 16:42:13 -0400

    built form input component and placed email and password fields within signinform component

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,54 +1,33 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
-// import { FormInput } from '../formFields';
+import { FormInput } from '../formFields';
 
-// class SignInForm extends Component {
-//     render() {
-//         const { className } = this.props;
-//         return (
-//             <form className={`${className} sign-in-form`}>
-//                 <Field className='sign-in-form__email' name='email' component={FormInput} />
-//             </form>
-//         )
-//     }
-// }
+class SignInForm extends Component {
+    render() {
+        const { className } = this.props;
+        return (
+            <form className={`${className} sign-in-form`}>
+                <Field className='sign-in-form__email'
+                    type='email'
+                    title='Email'
+                    placeholder='Email'
+                    name='email'
+                    component={FormInput} />
+                <Field className='sign-in-form__password'
+                    type='password'
+                    title='Password'
+                    placeholder='Password'
+                    name='password'
+                    component={FormInput} />
+            </form>
+        )
+    }
+}
 
-// SignInForm = reduxForm({
-//     form: 'SignInForm'
-// })(SignInForm);
+SignInForm = reduxForm({
+    form: 'SignInForm'
+})(SignInForm);
 
-// export default SignInForm;
-
-
-
-// import React, { Component } from 'react';
-
-// import { reduxForm, Field } from 'redux-form';
-
-// import { FormInput } from '../formFields';
-
-// class SignInForm extends Component {
-//     render() {
-//         const { className } = this.props;
-//         return (
-//             <form className={`${className} sign-in-form`}>
-//                 <Field className='sign-in-form__email'
-//                     type='email'
-//                     title='Email'
-//                     placeholder='Email'
-//                     name='email'
-//                     component={FormInput} />
-//                 <Field className='sign-in-form__password'
-//                     type='password'
-//                     title='Password'
-//                     placeholder='Password'
-//                     name='password'
-//                     component={FormInput} />
-//             </form>
-//         )
-//     }
-// }
-
-// export default SignInForm;
+export default SignInForm;
