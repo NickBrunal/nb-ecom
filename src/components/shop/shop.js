10f75fd6436642796eaad9cee13d19c5ae5bc590
Nick Brunal <nickbrunal@gmail.com>
2019-11-09 19:18:49 -0500

    developed shop reducer  and fetch shop categories action creator

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -14,6 +14,7 @@ export class Shop extends Component {
             }
         ]
         this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories();
         // fetch shop products action creator
         // fetch navbar links
         // set navbar links
